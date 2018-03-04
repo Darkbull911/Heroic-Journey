37a5478ce00939a04303a55e229124f4068f7828
Charlie Brandt <Nomasp911@gmail.com>
2018-03-04 21:18:54 +0000

    Add code for wiring up wheels

--- index.js
+++ index.js
@@ -1,49 +1,99 @@
-var rp = require("rpio");
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var fs = require('fs');
-var url = require('url');
+const rp = require("rpio");
+const app = require('http').createServer(handler);
+const io = require('socket.io')(app);
+const fs = require('fs');
+const url = require('url');
 
-var FORWARD = 16
-var BACK = 15
-var LEFT = 18
-var RIGHT = 22
+const FRONT_LEFT_POS = 11;
+const FRONT_LEFT_NEG = 13;
+const FRONT_RIGHT_POS = 12;
+const FRONT_RIGHT_NEG = 16;
+const BACK_LEFT_POS = 19;
+const BACK_LEFT_NEG = 21;
+const BACK_RIGHT_POS = 22;
+const BACK_RIGHT_NEG = 24;
+
+const FRONT_LEFT = [FRONT_LEFT_POS, FRONT_LEFT_NEG];
+const FRONT_RIGHT = [FRONT_RIGHT_POS, FRONT_RIGHT_NEG];
+const BACK_LEFT = [BACK_LEFT_POS, BACK_LEFT_NEG];
+const BACK_RIGHT = [BACK_RIGHT_POS, BACK_RIGHT_NEG];
+
+const ALL_PINS = [
+	...FRONT_LEFT,
+	...FRONT_RIGHT,
+	...BACK_LEFT,
+	...BACK_RIGHT
+];
+const ALL_POS = [
+	FRONT_LEFT[0],
+	FRONT_RIGHT[0],
+	BACK_LEFT[0],
+	BACK_RIGHT[0]
+];
+const ALL_NEG = [
+	FRONT_LEFT[1],
+	FRONT_RIGHT[1],
+	BACK_LEFT[1],
+	BACK_RIGHT[1]
+];
+
+const initPin = pin => rp.open(pin, rp.OUTPUT, rp.LOW);
+
+const initPins = () => ALL_PINS.forEach(initPin);
+
+const pinOff = pin => {
+	console.log('Turning OFF pin: ' + pin);
+	rp.write(pin, rp.LOW)
+};
+
+const pinOn = pin => {
+	console.log('Turning ON pin: ' + pin);
+	rp.write(pin, rp.HIGH);
+};
+
+const allOff = () => ALL_PINS.forEach(pinOff);
+
+const goForward = () => {
+	allOff();
+	ALL_POS.forEach(pinOn);
+};
+
+const goBackwards = () => {
+	allOff();
+	ALL_NEG.forEach(pinOn);
+};
+
+const goLeft = () => {
+	allOff();
+	pinOn(FRONT_LEFT_NEG);
+	pinOn(FRONT_RIGHT_POS);
+	pinOn(BACK_LEFT_NEG);
+	pinOn(BACK_RIGHT_POS);
+};
+
+const goRight = () => {
+	allOff();
+	pinOn(FRONT_LEFT_POS);
+	pinOn(FRONT_RIGHT_NEG);
+	pinOn(BACK_LEFT_POS);
+	pinOn(BACK_RIGHT_NEG);
+};
 
 app.listen(8000);
 
-rp.open(FORWARD, rp.OUTPUT,rp.LOW);
-rp.open(BACK, rp.OUTPUT,rp.LOW);
-rp.open(LEFT, rp.OUTPUT,rp.LOW);
-rp.open(RIGHT, rp.OUTPUT,rp.LOW);
-/*
-while(true)
-{
-	off(rp);
-	rp.msleep(500);
-	on(rp);
-	rp.msleep(500);
-}
-*/
-off(rp, FORWARD);
-
+initPins();
 
 io.on('connection', socket => {
 	console.log('got connection');
 	
 	socket.on('OFF', () => {
-		off(rp, FORWARD);
-		off(rp, BACK);
-		off(rp, LEFT);
-		off(rp, RIGHT);
+		allOff();
 	});
 	
-	socket.on('FORWARD', () => on(rp, FORWARD));
-	socket.on('BACK', () => {
-		console.log('yep');
-		on(rp, BACK)
-	});
-	socket.on('LEFT', () => on(rp, LEFT));
-	socket.on('RIGHT', () => on(rp, RIGHT));
+	socket.on('FORWARD', goForward);
+	socket.on('BACK', goBackwards);
+	socket.on('LEFT', goLeft);
+	socket.on('RIGHT', goRight);
 });
 
 
@@ -63,15 +113,4 @@ function handler (req, res) {
 			res.writeHead(200);
 			res.end(data);
 		});
-}
-
-
-function on(rp, pin)
-{
-	rp.write(pin, rp.HIGH);
-}
-
-function off(rp, pin)
-{
-	rp.write(pin, rp.LOW);
 }
